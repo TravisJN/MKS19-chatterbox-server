848c78ba26cf9d605eadd6134911ac9f73793680
travisjn <tdizzletrav@gmail.com>
2015-06-29 15:17:43 -0700

    Passes all server tests but no Request Listener tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,3 +1,6 @@
+var url = require('url');
+
+var results = [];
 /*************************************************************
 
 You should implement your request handler function in this file.
@@ -11,6 +14,17 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var collectData = function(request, callback) {
+  var body = '';
+  request.on('data', function(chunk){
+    console.log('chunk: ', chunk);
+    body += chunk;
+  });
+  request.on('end', function() {
+    console.log('end');
+    callback(JSON.parse(body));
+  });
+}
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -27,7 +41,6 @@ var requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
 
   // The outgoing status.
   var statusCode = 200;
@@ -35,15 +48,67 @@ var requestHandler = function(request, response) {
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
 
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  headers['Content-Type'] = "application/JSON";
+
+  var parsedURL = url.parse(request.url).pathname;
+  parsedURL = parsedURL.split('/');
+
+  if (parsedURL[1] !== 'classes') {
+    statusCode = 404;
+    headers['Content-Type'] = "application/JSON";
+    response.writeHead(statusCode, headers);
+    response.end('Bad request');
+  }
+  if(request.method === 'POST') {
+    collectData(request, function(data){
+      results.push(data);
+    console.log("after POST: ", results);
+
+      statusCode = 201;
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({results: results}));
+
+    });
+  } else if(request.method === 'GET') {
+    response.writeHead(statusCode, headers);
+    console.log(results);
+    response.end(JSON.stringify({results: results}));
+  }
+
+  // var router = {
+  //   'GET': function() {
+  //   },
+  //   'POST': function() {
+  //     request.on('data', function(chunk) {
+  //       console.log('i"m here', chunk);
+  //     })
+  //
+  //     results.push('something');
+  //     statusCode = 201;
+  //   }
+  // }
+  //
+  // var parsedURL = url.parse(request.url).pathname;
+  // parsedURL = parsedURL.split('/');
+  // if(parsedURL[1] === 'classes') {
+  //   if(router[request.method]){
+  //     router[request.method]();
+  //   }
+  // } else if (parsedURL === '/classes/room1/') {
+  //
+  // }
+
+
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +117,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+
+  //response.end(JSON.stringify({results: results}));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,3 +137,4 @@ var defaultCorsHeaders = {
   "access-control-max-age": 10 // Seconds.
 };
 
+module.exports = requestHandler;
